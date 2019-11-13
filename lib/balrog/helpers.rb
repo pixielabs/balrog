c2c1d82ef7fd1fc18f4236a78fed4d1cceea95ad
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-11-13 15:33:59 +0000

    extract the conditional #authenticated? logic into a module

--- lib/balrog/helpers.rb
+++ lib/balrog/helpers.rb
@@ -1,24 +1,12 @@
+require_relative 'guard'
+
 # Helpers methods are made available in all controllers by the code in engine.rb.
 module Balrog::Helpers
+  include Balrog::Guard
+
   def authenticate_with_balrog!
-    unless authenticated? && still_valid?
+    unless authenticated?(session[:balrog])
       render 'balrog/gate', layout: nil
     end
-  end
-
-  private
-
-  # A method to check that the user has been authenticated.
-  def authenticated?
-    return false unless session[:balrog]
-    session[:balrog]['value'] == 'authenticated'
-  end
-
-  # A method to check that the authentication has not expired.
-  def still_valid?
-    # If the user did not set configured the Balrog session 
-    # to expire, the cookie is valid.
-    return true unless session[:balrog]['expiry_date']
-    DateTime.current < session[:balrog]['expiry_date']
   end
 end
--- lib/balrog/routes_middleware.rb
+++ lib/balrog/routes_middleware.rb
@@ -1,3 +1,5 @@
+require_relative 'guard'
+
 # Public: Balrog routes middleware that redirects the user to a security
 # gate unless the session includes { 'balrog' => 'authenticated' }.
 # 
@@ -11,34 +13,18 @@
 # mount Sidekiq::Web => '/sidekiq'
 
 class Balrog::RoutesMiddleware
+  include Balrog::Guard
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
-    @balrog_session = env['rack.session']['balrog']
-    unless authenticated? && still_valid?
+    unless authenticated?(env['rack.session']['balrog'])
       html = ApplicationController.renderer.render 'balrog/gate', layout: nil
       return [200, {"Content-Type" => "text/html"}, [html]]  
     end
     @app.call(env)
-  end 
-
-  private
-
-  # A method to check that the user has been authenticated.
-  def authenticated?
-    return false unless @balrog_session
-    @balrog_session['value'] == 'authenticated'
   end
-
-  # A method to check that the authentication has not expired.
-  def still_valid?
-    # If the user did not set configured the Balrog session 
-    # to expire, the cookie is valid.
-    return true unless @balrog_session['expiry_date']
-    DateTime.current < @balrog_session['expiry_date']
-  end
-
 end
 
