578c33fd62fdce3afb44a3f636e3816ca8744b7f
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-11-25 15:30:16 +0000

    friendly bools to help show Balrog gate.

--- lib/balrog/view_helpers.rb
+++ lib/balrog/view_helpers.rb
@@ -1,4 +1,4 @@
-# ViewHelpers methods are made available in all controllers by the code in engine.rb.
+# ViewHelpers methods are made available in all views by the code in engine.rb.
 module Balrog::ViewHelpers
   def balrog_logout_button(options = nil, html_options = nil)
     name = 'Logout'
@@ -12,5 +12,13 @@ module Balrog::ViewHelpers
     end 
 
     button_to(name, '/balrog/logout', html_options)
+  end
+
+  def show_balrog_password_prompt?
+    !!Balrog::Middleware.password_hash || !Balrog::Middleware.omniauth_config
+  end
+
+  def balrog_omniauth_configured?
+    !!Balrog::Middleware.omniauth_config
   end
 end
